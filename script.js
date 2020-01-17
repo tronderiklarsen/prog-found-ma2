4dbe1fb3902d08bcbd874de03be5e8ce000a4780
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-01-17 11:46:35 +0100

    finished ma2

---
+++ script.js
@@ -0,0 +1,67 @@
+// question 1
+
+// var petArray = ["cat", "cow", "dog"];
+
+// question 2 
+
+// console.log(petArray[1]);
+
+// question 3 
+
+// petArray.push("sheep");
+
+// question 4 
+
+// console.log(petArray.length);
+
+// question 5
+
+/*
+var catObject = {
+    name: "Rambo",
+    color: "black and white",
+    age: 8
+};
+*/
+
+
+// question 6
+
+//console.log(catObject.age);
+
+// question 7
+
+/*
+var catArray = [
+    {
+        name: "Rambo",
+        color: "black and white",
+        age: 8
+    }
+];
+*/
+
+// question 8
+
+/*
+for (var i = 0; i < catArray.length; i ++) {
+    console.log(catArray[i]);
+}
+*/
+
+// question 9
+
+/*
+function logToConsole(packAnimal) {
+    console.log(packAnimal);
+}
+*/
+
+// question 10
+
+// logToConsole("donkey");
+
+
+
+
+
